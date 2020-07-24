d2f9a04c2e67a9edfb3a190ccf24c59ddff31041
Julia Shybunka <julishybunka@gmail.com>
2020-07-24 11:30:23 +0300

    Create header

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.get('/', function(req, res) {
+    res.render("index")
+})
+
 
 
 app.listen(3000, function() {
