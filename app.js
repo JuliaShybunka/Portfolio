f5663a4b0c212d8ec9d30559db352133f3c228ae
Julia Shybunka <julishybunka@gmail.com>
2020-07-27 13:11:49 +0300

    create workDB

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const ejs = require('ejs');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/portfolioWorkDB', { useNewUrlParser: true, useUnifiedTopology: true });
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const workSchema = new mongoose.Schema({
+    image: String,
+    title: String,
+    link: String,
+});
+const Work = mongoose.model('Work', workSchema);
+
 
 app.get('/', function(req, res) {
+    Work.find({}, function(err, works) {
+        if (works.length === 0) {
+
+        } else {
+
+        }
+    })
     res.render("index")
 })
 
